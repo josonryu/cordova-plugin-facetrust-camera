db3bd485da10e781da873de8efd91b6df60031c9
徐孫龍 <fj5107iq@aa.jp.fujitsu.com>
2025-02-14 10:11:44 +0900

    1.5

--- src/windows/bizHandler.js
+++ src/windows/bizHandler.js
@@ -182,7 +182,7 @@ var bizHandler = {
                         writeErrorCallback(error);
                     }
                 }, writeErrorCallback);
-            });
+            }, resolveErrorCallback);
         }, resolveErrorCallback);
     },
     removeFile: function (filePath, successCallback, errorCallback) {
